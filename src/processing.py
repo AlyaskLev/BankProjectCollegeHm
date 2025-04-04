ddc0896db471e947b5dbb33e2be4e5ebcdb3d37d
Alya <kimc60578@gmail.com>
2025-04-04 11:57:23 +0300

    добавлена новая функция фильтрации по датам

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,5 @@
 from typing import Dict, List
 
-
-def filter_by_state(data: List[Dict], state: str = "EXECUTED") -> List[Dict]:
-    """Функция для фильтрации словарей по ключу 'state'"""
-    lower_state = state.lower()
-    return [item for item in data if item.get("state", "").lower() == lower_state]
-
-
 input_data = [
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
@@ -15,5 +8,19 @@ input_data = [
 ]
 
 
+def filter_by_state(data: List[Dict], state: str = "EXECUTED") -> List[Dict]:
+    """функция для фильтрации списка словарей по ключу 'state'"""
+    lower_state = state.lower()
+    return [item for item in data if item.get("state", "").lower() == lower_state]
+
+
 print(filter_by_state(input_data, state="executed"))
 print(filter_by_state(input_data, state="canceled"))
+
+
+def sort_by_date(date_list: List[Dict]) -> List[Dict]:
+    """функция для сортировки списка словарей по датам"""
+    return sorted(date_list, key=lambda x: x["date"], reverse=True)
+
+
+print(sort_by_date(input_data))
