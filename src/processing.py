1387e2d542d5a415510e3a5e897a708dd5b62177
Alya <kimc60578@gmail.com>
2025-04-04 11:42:56 +0300

    добавлена функция фильтрации словарей

---
+++ src/processing.py
@@ -0,0 +1,19 @@
+from typing import Dict, List
+
+
+def filter_by_state(data: List[Dict], state: str = "EXECUTED") -> List[Dict]:
+    """Функция для фильтрации словарей по ключу 'state'"""
+    lower_state = state.lower()
+    return [item for item in data if item.get("state", "").lower() == lower_state]
+
+
+input_data = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
+
+print(filter_by_state(input_data, state="executed"))
+print(filter_by_state(input_data, state="canceled"))
